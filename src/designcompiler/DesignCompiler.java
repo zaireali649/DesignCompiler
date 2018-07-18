45ee6fb44dc6120ba599786744effcbe98e6c211
Zaire Ali <zaireali649@gmail.com>
2018-07-17 20:24:58 -0400

    Init Commit

---
+++ src/designcompiler/DesignCompiler.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designcompiler;
+
+/**
+ *
+ * @author Ziggy
+ */
+public class DesignCompiler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
